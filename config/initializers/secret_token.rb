52b1f318f99c006dfee3de505430dc5636099b1d
administrator <administrator@administrator-OptiPlex-380.(none)>
2013-01-03 14:44:37 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyRepo::Application.config.secret_token = 'c9dd837c3ba9421bcb26eeec3604d7467bcf81acea398b40ad5f159dcbd8115db03d555a7742b227c12e6bd38e1d9aa8831e194f124b6f08fd57b5c369a71af6'
